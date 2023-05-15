b577cc841615f83f7ee81d2511a7900bc607f7f8
Mik33221 <miki103gekon@gmail.com>
2023-05-15 22:16:01 +0200

    Generacja przykład

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class Student {
+    public Student(){
+        health = 100;
+        luck = 0;
+        perception = 0;
+        System.out.println("Empty created");
+    }
+    public Student(int health, int luck, int perception){
+        this.health = health;
+        this.luck = luck;
+        this.perception = perception;
+        System.out.println("Student: health - "+health+" luck - "+luck+" perception - "+perception);
+    }
+    private int health;
+    private int luck;
+    private int perception;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public int getPerception() {
+        return perception;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+
+    public void setPerception(int perception) {
+        this.perception = perception;
+    }
 }
