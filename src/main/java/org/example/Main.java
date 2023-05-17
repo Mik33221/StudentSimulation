8ea406564a9e4a5d23af58f8a4c3bfc68c25ff32
Mik33221 <miki103gekon@gmail.com>
2023-05-17 16:38:05 +0200

    Student generation update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        StudentGenerator Liceum = new StudentGenerator();
-        List<Student> studentList = Liceum.CreateRandom(3);
+        StudentGenerator liceum = new StudentGenerator();
+        List<Student> studentList = liceum.CreateRandom(3);
+
+        Party party = new Party();
+        Study study = new Study();
+        Work work = new Work();
+        System.out.println(studentList.get(0).toString());
+
+        for(Student student:studentList){
+            ;
+        }
+
         //System.out.println(studentList.toString());
         //System.out.println(studentList.get(1).getHealth());
     }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -23,10 +23,23 @@ public class Student {
         return health;
     }
     public void setHealth(int health) {
-        this.health = health;
+        this.health =+ health;
+        if (this.health < 0) this.health = 0;
     }
     public HashMap getSkills(){
         return skills;
     }
+    public void ModifyThing(){
 
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", health=" + health +
+                ", skills=" + skills +
+                '}';
+    }
 }
