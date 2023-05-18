4ebd4dea82a3f81830cf99f38b3ff937d8dbc745
Mik33221 <miki103gekon@gmail.com>
2023-05-18 08:53:00 +0200

    POLYMORPHYSM !

--- src/main/java/org/example/StudyIT.java
+++ src/main/java/org/example/StudyIT.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class StudyIT extends Study{
+    @Override
+    public void ModifyStudent(Student student) {
+        student.getSkills().merge("IT",1,(oldValue, newValue) -> (int)oldValue + (int)newValue);
+    }
 }
--- src/main/java/org/example/StudyPhysics.java
+++ src/main/java/org/example/StudyPhysics.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class StudyPhysics extends Study{
+    @Override
+    public void ModifyStudent(Student student) {
+        student.getSkills().merge("Physics",1,(oldValue, newValue) -> (int)oldValue + (int)newValue);
+    }
 }
