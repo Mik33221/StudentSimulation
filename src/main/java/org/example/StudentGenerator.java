abb62407881a47b0785e79ca82f419e8676435bb
Mik33221 <miki103gekon@gmail.com>
2023-06-10 16:38:03 +0200

    Predispositions w student StudentGenerator update WIP

--- src/main/java/org/example/StudentGenerator.java
+++ src/main/java/org/example/StudentGenerator.java
@@ -1,15 +1,19 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-public class StudentGenerator {
+public class    StudentGenerator {
     static private final List<String> names = Arrays.asList("Kaylen", "Jace", "Jimmie", "Liam", "Sidney", "Linsey", "Leila", "Oswaldo", "Royce", "Willie", "Clarissa", "Robyn", "Kristin", "Donte", "Maurice", "Marquez", "Humberto", "Julia", "Drew", "Brayan", "Shamar", "Cassie", "Raekwon", "Anita", "Trayvon", "Ben", "Marie", "Codey", "Beyonce", "Erykah", "Travion", "Lucinda", "Jalyn", "Tionna", "Adam", "Jaclyn", "Ashlynn", "Ruben", "Seth", "Tristen");
     static private final List<String> surNames = Arrays.asList("John", "Eggleston", "Skinner", "Reedy", "Serna", "Harris", "Pate", "Bruce", "Weems", "Crouch", "Mireles", "Rosenthal", "Billings", "Chaney", "Rupp", "Gomez", "Keel", "Jarrett", "Rees", "Gerard", "Winston", "Weston", "Santana", "Forrest", "Barker", "Dennis", "Kern", "Knoll", "Houser", "Falcon", "Sandberg", "Zepeda", "Mora", "Mixon", "Bunting", "Caron", "Goebel", "Coronado", "Van", "Mathews");
 
     static int indexNumber = 1;
+    HashMap<String, Integer> rollPredispositions(boolean type){
+        if(type==true){
+
+        }else{
+
+        }
+    }
 
     public static List<Student> createRandomStudent(int count) {
         Random random = new Random();
@@ -21,7 +25,8 @@ public class StudentGenerator {
                             names.get(random.nextInt(names.size())),
                             surNames.get(random.nextInt(surNames.size())),
                             random.nextInt(80, 100),
-                            indexNumber++
+                            indexNumber++,
+                            predispositions
                     );
             students.add(newStudent);
         }
