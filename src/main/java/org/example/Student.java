4704a1ae61efbe747163d02a2e934c0419ec96f7
Mik33221 <miki103gekon@gmail.com>
2023-06-10 16:37:59 +0200

    Predispositions w student StudentGenerator update WIP

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,14 +1,18 @@
 package org.example;
 
 
+import java.util.HashMap;
+
 public class Student extends Person {
     final int indexNumber;
     final StudentState state;
+    private final HashMap<String, Integer> predispositions; //TOOD na tej podstawie studenci mają decydować jaki event zrobić
 
-    public Student(String name, String surName, int health, int indexNumber) {
+    public Student(String name, String surName, int health, int indexNumber, HashMap<String, Integer> predispositions) {
         super(name, surName, health);
         this.indexNumber = indexNumber;
         this.state = StudentState.ACTIVE;
+        this.predispositions = (HashMap<String, Integer>) predispositions.clone();
     }
 
     @Override
