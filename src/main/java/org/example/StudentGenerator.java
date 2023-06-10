653a6375b11a50441e275dcb497996800c6dc3b1
Mik33221 <miki103gekon@gmail.com>
2023-06-10 17:56:52 +0200

    realistyczne indeksy

--- src/main/java/org/example/StudentGenerator.java
+++ src/main/java/org/example/StudentGenerator.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class StudentGenerator {
     static private final List<String> names = Arrays.asList("Kaylen", "Jace", "Jimmie", "Liam", "Sidney", "Linsey", "Leila", "Oswaldo", "Royce", "Willie", "Clarissa", "Robyn", "Kristin", "Donte", "Maurice", "Marquez", "Humberto", "Julia", "Drew", "Brayan", "Shamar", "Cassie", "Raekwon", "Anita", "Trayvon", "Ben", "Marie", "Codey", "Beyonce", "Erykah", "Travion", "Lucinda", "Jalyn", "Tionna", "Adam", "Jaclyn", "Ashlynn", "Ruben", "Seth", "Tristen");
     static private final List<String> surNames = Arrays.asList("John", "Eggleston", "Skinner", "Reedy", "Serna", "Harris", "Pate", "Bruce", "Weems", "Crouch", "Mireles", "Rosenthal", "Billings", "Chaney", "Rupp", "Gomez", "Keel", "Jarrett", "Rees", "Gerard", "Winston", "Weston", "Santana", "Forrest", "Barker", "Dennis", "Kern", "Knoll", "Houser", "Falcon", "Sandberg", "Zepeda", "Mora", "Mixon", "Bunting", "Caron", "Goebel", "Coronado", "Van", "Mathews");
-    static int indexNumber = 1;
+    static int indexNumber = 270000;
 
     static HashMap<String, Integer> RollPredispositions(boolean type) {
         Random random = new Random();
