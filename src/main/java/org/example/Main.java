ae4e4636f76cc661d26e11411744aa9fad102c8a
Anna Ilnicka-Wandycz <183511@student.pwr.edu.pl>
2023-06-14 01:21:50 +0200

    wyswietlenie statystyk zamiast statusów poszczególnych osób

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         final int semesterDays = 105; // TOOD: pobierz liczbę od użytkownika
-        final int initialNumberOfStudents = 8; // TOOD: pobierz liczbę od użytkownika
+        final int initialNumberOfStudents = 40; // TOOD: pobierz liczbę od użytkownika
 
         Semester semester = new Semester(semesterDays, StudentGenerator.createRandomStudent(initialNumberOfStudents));
-        semester.printStudentStateList();
         semester.run();
-        semester.printStudentStateList();
     }
 
 }
