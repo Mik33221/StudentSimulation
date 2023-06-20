09c16048830f5944fbf9351729077de15ce292e7
Mik33221 <miki103gekon@gmail.com>
2023-06-20 23:29:49 +0200

    komentarze Exam

--- src/main/java/org/example/Exam.java
+++ src/main/java/org/example/Exam.java
@@ -3,18 +3,31 @@ package org.example;
 import org.example.events.Event;
 
 
+/**
+ * Exam przyjmuje wartości umiejętności studentów
+ * na ich podstawie przyporządkowuje oceny za poszczególne przedmioty
+ */
 public class Exam implements Event {
 
     final private int mathSkillValue;
     final private int physicsSkillValue;
     final private int itSkillValue;
 
+    /**
+     * @param mathSkillValue    - wartość wiedzy z matematyki
+     * @param physicsSkillValue - wartość wiedzy z fizyki
+     * @param itSkillValue      - wartość wiedzy z IT
+     */
     public Exam(int mathSkillValue, int physicsSkillValue, int itSkillValue) {
         this.mathSkillValue = mathSkillValue;
         this.physicsSkillValue = physicsSkillValue;
         this.itSkillValue = itSkillValue;
     }
 
+    /**
+     * Wypełnia tablicę ocen studenta ocenami z egzaminów
+     * @param student
+     */
     @Override
     public void modifyStudent(Student student) {
         int[] examGrades = new int[3];
@@ -36,12 +49,16 @@ public class Exam implements Event {
             student.setState(StudentState.DELETED);
         }
         student.setExamGrades(examGrades);
-      
+
         ExamResultsPrinter resultsPrinter = new ExamResultsPrinter(student.indexNumber, examGrades);
         resultsPrinter.printResults();
 
     }
 
+    /**
+     * @param points wartosc punktów z egzaminu
+     * @return ocena z poszczególnego egzaminu
+     */
     private int calGrade(int points) {
         if (points >= 50) {
             return 5;
