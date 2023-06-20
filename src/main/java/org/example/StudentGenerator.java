495ebd6529bcaa6d3859d848b86d44aca64ce793
Mik33221 <miki103gekon@gmail.com>
2023-06-20 23:47:36 +0200

    komentarze StudentGenerator

--- src/main/java/org/example/StudentGenerator.java
+++ src/main/java/org/example/StudentGenerator.java
@@ -2,11 +2,19 @@ package org.example;
 
 import java.util.*;
 
+/**
+ * Klasa konieczna do wygenerowania studentów biorących udział w symulacji
+ * Generuje studenta nadając mu cechy z klasy Person jak i Student
+ */
 public class StudentGenerator {
     static private final List<String> names = Arrays.asList("Kaylen", "Jace", "Jimmie", "Liam", "Sidney", "Linsey", "Leila", "Oswaldo", "Royce", "Willie", "Clarissa", "Robyn", "Kristin", "Donte", "Maurice", "Marquez", "Humberto", "Julia", "Drew", "Brayan", "Shamar", "Cassie", "Raekwon", "Anita", "Trayvon", "Ben", "Marie", "Codey", "Beyonce", "Erykah", "Travion", "Lucinda", "Jalyn", "Tionna", "Adam", "Jaclyn", "Ashlynn", "Ruben", "Seth", "Tristen");
     static private final List<String> surNames = Arrays.asList("John", "Eggleston", "Skinner", "Reedy", "Serna", "Harris", "Pate", "Bruce", "Weems", "Crouch", "Mireles", "Rosenthal", "Billings", "Chaney", "Rupp", "Gomez", "Keel", "Jarrett", "Rees", "Gerard", "Winston", "Weston", "Santana", "Forrest", "Barker", "Dennis", "Kern", "Knoll", "Houser", "Falcon", "Sandberg", "Zepeda", "Mora", "Mixon", "Bunting", "Caron", "Goebel", "Coronado", "Van", "Mathews");
     static int indexNumber = 270000;
-
+    /**
+     *
+     * @param type pobieramy informacje czy humanista czy ścisłowiec
+     * @return zwraca hashmapę predyspozycji dla pojedyńczego studenta
+     */
     static HashMap<String, Integer> RollPredispositions(boolean type) {
         Random random = new Random();
         HashMap<String, Integer> pred = new HashMap<>();
@@ -24,7 +32,11 @@ public class StudentGenerator {
         }
         return pred;
     }
-
+    /**
+     *
+     * @param count liczba studentów do wygenerowania
+     * @return lista wszystkich studentów
+     */
     public static List<Student> createRandomStudent(int count) {
         Random random = new Random();
         List<Student> students = new ArrayList<>();
